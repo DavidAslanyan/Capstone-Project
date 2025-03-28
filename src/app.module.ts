07959d02e7abfc6048672cec5742389028b2c903
DavidAslanyan <david.aslanyan@icketi.com>
2025-03-28 20:52:56 +0400

    Setted up websockets and socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './database/typeorm';
 import { CoreModule } from './core/infrastructure/modules/core.module';
+import { GatewayModule } from './user/presentation/gateways/gateway.module';
 import { UserModule } from './user/infrastructure/modules/user.module';
 import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    GatewayModule,
     CoreModule,
     UserModule,
     ConfigModule.forRoot({
